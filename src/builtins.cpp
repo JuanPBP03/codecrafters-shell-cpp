497d7a4a59ad87a7b2dc26178d644ba1aefef4df
JuanPBP03 <juanpablobecerra103@gmail.com>
2025-10-08 20:25:19 -0400

    added builtins.cpp and myshell header file

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -2,6 +2,8 @@
 #define _BUILTINS_
 #include "myshell.hpp"
 
+sh_status_t echo(std::stringstream& input);
+
 sh_status_t echo(std::stringstream& input){
 
     std::cout << input.str() << std::endl;
