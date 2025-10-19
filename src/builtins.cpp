e3b7e61a20024b81cb51900bc81397e7a0042ef3
JuanPBP03 <juanpablobecerra103@gmail.com>
2025-10-19 01:19:07 -0400

    added PATH variable searching to type command

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -21,7 +21,7 @@ sh_status_t type(std::stringstream& args){
     }else if(!PATH.empty()){
             //std::cout<<"PATH FOUND"<< std::endl;
             for(const auto& dir : PATH){
-                printf("DIR: %s", dir);
+                printf("DIR: %s", dir.string());
                 if(std::filesystem::exists(dir / (arg))){
                     std::cout << arg << " is " << (dir/arg).string() << std::endl;
                     return STATUS_OK;
